748d3b53c9b57fca0ab9ec9c788fe60bda29c309
Todd Hendricks <hendricks.ta@gmail.com>
2017-03-06 12:19:18 -0600

    More JavaScript added to govern sequence and logic of the Simon pattern.

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -14,8 +14,8 @@ function generateRandomNumber() {
 }
 
 
-$("button#start").on("click", function() {
-	generateRandomNumber();
+$("#start").on("click", function() {
+	newRound();
 });
 
 
@@ -32,57 +32,72 @@ function getRandomSquare(){
 
 // need to add functions here.
 
-function animate(){
-	var randomSquare = getRandomSquare();
-	randomSquare.animate({opacity: .4},80).animate({opacity: 1},80);	
+function newRound (){
+	masterPattern.push(getRandomSquare());
+	console.log(masterPattern);
+	simonAnimateSequence();
 }
 
-function simonSequence(){
+function simonAnimateSequence () {
+	var count = 0;
+	var max = masterPattern.length; // set to master pattern.length
+	var interval = 1000; // interval time in milliseconds
 
+	var intervalId = setInterval(function () {
+    if (count >= max) {
+        clearInterval(intervalId);
+        alert('Congrats! You have passed the first round!');
+    } else {
+        count++;
+        animateSquares(masterPattern[count]);
+        console.log("Fired");
+    }
+	}, interval);
 }
 
+function animateSquares (element) {
+	$(".square").animate({opacity: .4},80).animate({opacity: 1},80);	
+}
 
-
-	// masterPattern.push(random);
 
 // Aesthetics: JQuery page effects, decoration, interactivity
 
 
-$(".red").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
+$(".square").on("click", function(){
+	animateSquares($(this));
 });
 
-$(".blue").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
-});
+// $(".blue").on("click", function(){
+// 	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
+// });
 
-$(".yellow").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
-});
+// $(".yellow").on("click", function(){
+// 	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
+// });
 
-$(".green").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
-});
+// $(".green").on("click", function(){
+// 	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
+// });
 
-$(".red").hover(function() {
+$(".square").hover(function() {
 	$(this).fadeOut (100);
 	$(this).fadeIn (300);
 });
 
-$(".blue").hover(function() {
-	$(this).fadeOut (100);
-	$(this).fadeIn (300);
-});
+// $(".blue").hover(function() {
+// 	$(this).fadeOut (100);
+// 	$(this).fadeIn (300);
+// });
 
-$(".green").hover(function() {
-	$(this).fadeOut (100);
-	$(this).fadeIn (300);
-});
+// $(".green").hover(function() {
+// 	$(this).fadeOut (100);
+// 	$(this).fadeIn (300);
+// });
 
-$(".yellow").hover(function() {
-	$(this).fadeOut (100);
-	$(this).fadeIn (300);
-});
+// $(".yellow").hover(function() {
+// 	$(this).fadeOut (100);
+// 	$(this).fadeIn (300);
+// });
 
 
 
