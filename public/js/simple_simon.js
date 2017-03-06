2cb28c43d1eaac28a1e86ed6946513cace688554
Todd Hendricks <hendricks.ta@gmail.com>
2017-03-06 15:19:19 -0600

    Added rough code of user interactivity function.

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,12 +1,10 @@
-
 "use strict";
 
-// Functionality: variables, logic, rules
+// The code below establishes the logic, and the sequence of the squares lighting up.
 
 var masterPattern = [];
 
 var random;
-
 
 function generateRandomNumber() {
 	random = Math.floor(Math.random()* 4);
@@ -14,8 +12,8 @@ function generateRandomNumber() {
 }
 
 
-$("button#start").on("click", function() {
-	generateRandomNumber();
+$("#start").on("click", function() {
+	newRound();
 });
 
 
@@ -30,61 +28,72 @@ function getRandomSquare(){
 	return squares[RandomIndex];
 }
 
-function animate(){
-	var randomSquare = getRandomSquare();
-	randomSquare.animate({opacity: .4},80).animate({opacity: 1},80);	
+
+function newRound (){
+	masterPattern.push(getRandomSquare());
+	console.log(masterPattern);
+	simonAnimateSequence();
 }
 
-function simonSequence(){
+function simonAnimateSequence () {
+	var count = 0;
+	var max = masterPattern.length; // set to master pattern.length
+	var interval = 1000; // interval time in milliseconds
 
+	var intervalId = setInterval(function () {
+    if (count >= max) {
+        clearInterval(intervalId);
+        alert('Congrats! You have passed the first round!');
+    } else {
+        animateSquares(masterPattern[count]);
+        count++;
+    }
+	}, interval);
 }
 
 
+function animateSquares (element) {
+	element.animate({opacity: .4},80).animate({opacity: 1},80);	
+}
 
-	// masterPattern.push(random);
 
-// Aesthetics: JQuery page effects, decoration, interactivity
-
-
-$(".red").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
+$(".square").on("click", function(){
+	animateSquares($(this));
 });
 
-$(".blue").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
-});
 
-$(".yellow").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
-});
-
-$(".green").on("click", function(){
-	$(this).animate({opacity: .4},80).animate({opacity: 1},80);	
-});
-
-$(".red").hover(function() {
-	$(this).fadeOut (100);
-	$(this).fadeIn (300);
-});
-
-$(".blue").hover(function() {
-	$(this).fadeOut (100);
-	$(this).fadeIn (300);
-});
-
-$(".green").hover(function() {
-	$(this).fadeOut (100);
-	$(this).fadeIn (300);
-});
-
-$(".yellow").hover(function() {
+$(".square").hover(function() {
 	$(this).fadeOut (100);
 	$(this).fadeIn (300);
 });
 
 
+// The code below defines the user interactivity and how failure takes place.
 
 
+function userTurn () {
+	var clickCount = 0;
+
+		$(".square").on("click", function(){
+			var clickedId = "#" + $(this).attr("id");
+			console.log("Clicked: " + clickedId);
+			console.log("Array: " + memoryArray[clickCount]);
+			simonAnimateSequence(clickedId, 180);
+			console.log(this);
+			if (clickedId == memoryArray[clickCount]){
+			if (clickCount == memoryArray.length -1){
+				newMemory();
+				clickCount = 0;
+				$(".square").off("click");
+			} else {
+				clickCount++;
+			}
+		}else{
+			memoryArray = [];
+			alert("Game over!");
+		}
+	})
+}
 
 
 
